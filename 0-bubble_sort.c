43e006b11a8badde574765a44797277232cb8b23
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-04-25 16:05:50 +0300

    0-bubble_sort.c - whitespaces removed

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,41 +1,46 @@
 #include "sort.h"
+
 /**
- * bubble_sort - function that sorts an array of integers in ascending order
- * using the Bubble sort algorithm
- * @array: pointer to the array to be sorted
- * @size: size of the array to be sorted
- * Return: nothing
+ * swap_ints - Swap two integers in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
+ */
+void swap_ints(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * bubble_sort - Sort an array of integers in ascending order.
+ * @array: An array of integers to sort.
+ * @size: The size of the array.
+ *
+ * Description: Prints the array after each swap.
  */
 void bubble_sort(int *array, size_t size)
 {
-
-
-	size_t i = 0;
-	size_t j = 0;
-	int temp;
+	size_t i, len = size;
+	bool bubbly = false;
 
 	if (array == NULL || size < 2)
 		return;
-	if (array)
+
+	while (bubbly == false)
 	{
-		for (i = 0; i < size - 1; i++)
+		bubbly = true;
+		for (i = 0; i < len - 1; i++)
 		{
-			for (j = 0; j < size - i - 1; ++j)
+			if (array[i] > array[i + 1])
 			{
-				if (array[j] > array[j + 1])
-				{
-					temp = array[j];
-					array[j] = array[j + 1];
-					array[j + 1] = temp;
-					print_array(array, size);
-				}
+				swap_ints(array + i, array + i + 1);
+				print_array(array, size);
+				bubbly = false;
 			}
-
 		}
-
-	}
-	else
-	{
-		return;
+		len--;
 	}
 }
--- sort.h
+++ sort.h
@@ -1,8 +1,19 @@
-# ifndef SORT_H
-# define SORT_H
+#ifndef SORT_H
+#define SORT_H
 
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * enum bool - Enumeration of Boolean values.
+ * @false: Equals 0.
+ * @true: Equals 1.
+ */
+typedef enum bool
+{
+	false = 0,
+	true
+} bool;
 
 /**
  * struct listint_s - Doubly linked list node
@@ -18,9 +29,11 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-
+/* Printing helper functions */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+
+/* Sorting algoritms */
 void bubble_sort(int *array, size_t size);
 
-# endif /* SORT_H */
+#endif /* SORT_H */
