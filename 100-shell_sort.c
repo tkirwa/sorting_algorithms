8c36d7e8277310e7b9ece8d28a5ff892d3a41044
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-04-26 00:02:00 +0300

    100-shell_sort.c

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -8,25 +8,25 @@
  */
 void shell_sort(int *array, size_t size)
 {
-    size_t gap = 1, i, j;
-    int temp;
+size_t gap = 1, i, j;
+int temp;
 
-    if (array == NULL || size < 2)
-        return;
+if (array == NULL || size < 2)
+return;
 
-    while (gap < size / 3)
-        gap = gap * 3 + 1;
+while (gap < size / 3)
+gap = gap * 3 + 1;
 
-    while (gap > 0)
-    {
-        for (i = gap; i < size; i++)
-        {
-            temp = array[i];
-            for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
-                array[j] = array[j - gap];
-            array[j] = temp;
-        }
-        gap = (gap - 1) / 3;
-        print_array(array, size);
-    }
+while (gap > 0)
+{
+for (i = gap; i < size; i++)
+{
+temp = array[i];
+for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
+array[j] = array[j - gap];
+array[j] = temp;
+}
+gap = (gap - 1) / 3;
+print_array(array, size);
+}
 }
