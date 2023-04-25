e56cd6c2894da15aa0e7e20dee4a18af673c1729
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-04-25 14:55:36 +0300

    0-bubble_sort.c - whitespaces removed

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,31 +1,41 @@
 #include "sort.h"
-
 /**
- * bubble_sort - sorts an array of integers in ascending order
- *               using the Bubble sort algorithm
- *
- * @array: pointer to the integer array to sort
- * @size: number of elements in the array
+ * bubble_sort - function that sorts an array of integers in ascending order
+ * using the Bubble sort algorithm
+ * @array: pointer to the array to be sorted
+ * @size: size of the array to be sorted
+ * Return: nothing
  */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, j;
-    int tmp;
 
-    if (array == NULL || size < 2)
-        return;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        for (j = 0; j < size - i - 1; j++)
-        {
-            if (array[j] > array[j + 1])
-            {
-                tmp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = tmp;
-                print_array(array, size);
-            }
-        }
-    }
+	size_t i = 0;
+	size_t j = 0;
+	int temp;
+
+	if (array == NULL || size < 2)
+		return;
+	if (array)
+	{
+		for (i = 0; i < size - 1; i++)
+		{
+			for (j = 0; j < size - i - 1; ++j)
+			{
+				if (array[j] > array[j + 1])
+				{
+					temp = array[j];
+					array[j] = array[j + 1];
+					array[j + 1] = temp;
+					print_array(array, size);
+				}
+			}
+
+		}
+
+	}
+	else
+	{
+		return;
+	}
 }
