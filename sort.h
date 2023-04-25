1ec61993acff5bf07f892cf60120f3fc3102fe04
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-04-25 23:14:26 +0300

    0-bubble_sort.c, 0-O

--- sort.h
+++ sort.h
@@ -1,7 +1,8 @@
 #ifndef SORT_H
 #define SORT_H
 
-#include <stdlib.h>
+#include <stddef.h>
+
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -11,18 +12,16 @@
  */
 typedef struct listint_s
 {
-	const int n;
-	struct listint_s *prev;
-	struct listint_s *next;
+    const int n;
+    struct listint_s *prev;
+    struct listint_s *next;
 } listint_t;
 
 void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
-void print_list(const listint_t *list);
-listint_t *swap_node(listint_t *node, listint_t **list);
+void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 
-
-#endif
+#endif /* SORT_H */
