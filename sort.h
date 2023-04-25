4175c6d0b588e8b5566a284dd5e5740067938d2c
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-04-25 23:22:44 +0300

    1-insertion_sort_list.c

--- sort.h
+++ sort.h
@@ -17,6 +17,8 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
