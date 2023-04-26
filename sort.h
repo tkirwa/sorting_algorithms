4cce5fcde0de817e4310ff30e4693a563e80676a
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-04-26 10:07:30 +0300

    3-quick_sort.c, 3-O

--- sort.h
+++ sort.h
@@ -28,7 +28,12 @@ void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+
 void quick_sort(int *array, size_t size);
+void sort_alg(int *arr, int left, int right, size_t size);
+int split(int *arr, int left, int right, size_t size);
+
+
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
